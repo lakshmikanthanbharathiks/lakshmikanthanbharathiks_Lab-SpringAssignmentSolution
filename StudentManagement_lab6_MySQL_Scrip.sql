284fa02a2eac948c12a4a4fdad63812dd2f4e8da
Lakshmi Kanthan Bharathi K S <101494062+lakshmikanthanbharathiks@users.noreply.github.com>
2022-07-16 17:10:31 +0530

    Add files via upload

--- StudentManagement_lab6_MySQL_Scrip.sql
+++ StudentManagement_lab6_MySQL_Scrip.sql
@@ -1,1 +1,9 @@
-SELECT * FROM student_management.roles;
+create database student_management;
+insert into roles values(1,"ADMIN");
+insert into roles values(2,"STUDENT");
+insert into users values(1,"$2a$12$T4wfzf6eybSavqc9DbCSp.fEmOti2hM8UdDvYY8WCs5OYw18BlS1W","admin1");
+insert into users values(2,"$2a$12$T4wfzf6eybSavqc9DbCSp.fEmOti2hM8UdDvYY8WCs5OYw18BlS1W","student1");
+insert into users values(3,"$2a$12$T4wfzf6eybSavqc9DbCSp.fEmOti2hM8UdDvYY8WCs5OYw18BlS1W","student2");
+insert into user_roles values(1,1);
+insert into user_roles values(2,2);
+insert into user_roles values(3,2);
